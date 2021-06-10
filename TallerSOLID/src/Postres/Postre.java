1a81a6ea20e217318eddfafbdf93751d673b66c6
Viviana Vera Falconí <vivianavera03@gmail.com>
2021-06-10 10:20:58 -0500

    Literal 1 y 2.

---
+++ TallerSOLID/src/Postres/Postre.java
@@ -0,0 +1,49 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Postres;
+
+import Adicionales.Aderezo;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Viviana Vera Falconi
+ */
+public class Postre {
+    
+     private String sabor;
+    private double precioParcial;
+    private ArrayList<Aderezo> aderezos;
+    
+    public Postre(String sabor){
+        aderezos= new ArrayList<>();
+        this.sabor=sabor;
+        this.precioParcial = 15.55;
+    }
+    
+    public double calcularPrecioFinal(){
+        double precioFinal;
+        precioFinal=(precioParcial+(precioParcial*0.12))+(aderezos.size()*0.50);
+        return precioFinal;
+    }
+
+    public ArrayList<Aderezo> getAderezos() {
+        return aderezos;
+    }
+    
+    public void anadirAderezo(Aderezo aderezo){
+        this.getAderezos().add(aderezo);
+    }
+
+    @Override
+    public String toString() {
+        return "Pastel{" + "sabor=" + sabor + ", precioParcial=" + precioParcial + ", aderezos=" + aderezos + '}';
+    }
+    public String showPrecioFinal(){
+        return "Precio Final: $ " + calcularPrecioFinal();
+    }
+    
+}
